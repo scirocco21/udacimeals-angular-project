7558d30c2605be2ed1321451d0304f56b6fb50c4
Sebastian <sebgertz@gmail.com>
2019-05-05 15:30:07 -0400

    add controller menu

---
+++ app/scripts/controllers/menu.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name udaciMealsApp.controller:MenuCtrl
+ * @description
+ * # MenuCtrl
+ * Controller of the udaciMealsApp
+ */
+angular.module('udaciMealsApp')
+  .controller('MenuCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
