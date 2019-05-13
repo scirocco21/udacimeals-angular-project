683f41373d32f138208274815a4ba595072d0c30
Sebastian <sebgertz@gmail.com>
2019-05-13 14:01:30 -0400

    add foodfinder service to app

---
+++ app/scripts/services/foodfinder.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name udaciMealsApp.foodFinder
+ * @description
+ * # foodFinder
+ * Service in the udaciMealsApp.
+ */
+angular.module('udaciMealsApp')
+  .service('foodFinder', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
